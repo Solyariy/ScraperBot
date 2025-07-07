e8180402beeb339588566b4369bffd51ce3fa9ae
Oleksandr Skoromnov <solalex005@gmail.com>
2025-07-07 18:14:27 +0300

    ready

--- app/cron_scripts/scrap_and_parse_saz.py
+++ app/cron_scripts/scrap_and_parse_saz.py
@@ -5,14 +5,14 @@ from ..emulator import Parser, Scraper
 
 def scrap_and_parse(with_options: bool = False):
     with get_postgres_manager() as session:
-        dao = DisciplineDao(session=session)
+        # dao = DisciplineDao(session=session)
         with Scraper(Config.SAZ_INSTRUCTION, with_options=with_options) as sc:
             for url in Config.SAZ_INSTRUCTION.URLS_TO_PARSE:
                 sc.connect_main_page(url)
-                parser = Parser(sc.page_source)
-                all_disc = parser.parse().all_disciplines
-                dao.insert_all(all_disc)
+                # parser = Parser(sc.page_source)
+                # all_disc = parser.parse().all_disciplines
+                # dao.insert_all(all_disc)
 
 
 if __name__ == '__main__':
-    scrap_and_parse(with_options=True)
+    scrap_and_parse(with_options=False)
